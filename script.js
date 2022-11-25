4d3653da41e488f872e373883773eb91dd8eb421
MishaKollen <mikhailkollen@gmail.com>
2022-11-25 16:15:25 +0300

    Change styles of buttons

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0];
-const navbarLinks = document.getElementsByClassName('navbar-links')[0];
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 
-toggleButton.addEventListener('click', () => {
- navbarLinks.classList.toggle('active');
-}
-);
+toggleButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+});
+
+let hideBtn = [...document.querySelectorAll(".hideBtn")];
+
+hideBtn.forEach((button) => {
+  button.addEventListener("click", () => {
+    button.parentElement.open = false;
+  });
+});
